9e250230b901c46c37067a0ef3ce165ee9294b40
virtustron <virtustron@gmail.com>
2021-10-24 18:43:55 +0300

    Add g++ compilation of cpp-file

--- compiler/src/bin/compiler_main.rs
+++ compiler/src/bin/compiler_main.rs
@@ -1,21 +1,30 @@
+
 #![allow(unused)]
 
+use std::env;
 use std::process::Command;
 use std::io::{self, Write};
 
-fn main() {
+
+fn main() -> std::io::Result<()> {
     println!("Hello from [compiler] crate!");
+
+    let path = env::current_dir()?;
+    //println!("The current directory is {}", path.display());
+    
     
     let output = Command::new("g++")
-                .args(["-o", "test", "test.cpp"])
-                .spawn();
+                .current_dir(path)
+                .args(["-o", "./src/bin/test", "./src/bin/test.cpp"])
+                .output()
+                .expect("failed to execute process");
     
 
-    //println!("{}", output.err().unwrap());
-
-    println!("status: {}", output.status);
+    println!("Compiling status: {}", output.status);
     io::stdout().write_all(&output.stdout).unwrap();
     io::stderr().write_all(&output.stderr).unwrap();
 
-    assert!(output.status.success());    
+    assert!(output.status.success()); 
+    
+    Ok(())
 }
