aa56136fa2cfb0fb21d60eaaf59fb64f46c82754
virtustron <virtustron@gmail.com>
2021-10-24 18:20:14 +0300

    Trying to compile cpp

--- compiler/src/bin/compiler_main.rs
+++ compiler/src/bin/compiler_main.rs
@@ -1,5 +1,21 @@
-// read Chapter 8 - The src/bin Directory
+#![allow(unused)]
+
+use std::process::Command;
+use std::io::{self, Write};
 
 fn main() {
     println!("Hello from [compiler] crate!");
+    
+    let output = Command::new("g++")
+                .args(["-o", "test", "test.cpp"])
+                .spawn();
+    
+
+    //println!("{}", output.err().unwrap());
+
+    println!("status: {}", output.status);
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stderr().write_all(&output.stderr).unwrap();
+
+    assert!(output.status.success());    
 }
