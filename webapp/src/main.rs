d4f3c5437247e646ab776ea3972831e57bb46034
AndriiSerbeniuk <andriiserbeniuk@gmail.com>
2021-10-24 21:56:07 +0300

    Initialized basci rocket application

--- webapp/src/main.rs
+++ webapp/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+// TODO: receive application code to compile
+#[post("/submit", data = "<code>")]
+fn post_submit(code: String) -> String
+{
+    // TODO: send back compiler file
+    "i work: ".to_owned() + &code
+}
+
+#[launch]
+fn rocket() -> _ 
+{
+    // I'm not sure about FileServer, but it seems safe.
+    // And official tutorials use it too
+    rocket::build()
+        .mount("/", rocket::fs::FileServer::from("static/"))
+        .mount("/", routes![post_submit])
 }
