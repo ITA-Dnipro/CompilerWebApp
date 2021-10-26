d3c74266ecc25fbfa71cbb04c82cde2c94841ea8
AndriiSerbeniuk <andriiserbeniuk@gmail.com>
2021-10-27 00:00:58 +0300

    Added JSONs hadling

--- webapp/src/main.rs
+++ webapp/src/main.rs
@@ -1,11 +1,51 @@
 #[macro_use] extern crate rocket;
 
-// TODO: receive application code to compile
-#[post("/submit", data = "<code>")]
-fn post_submit(code: String) -> String
+use rocket::serde::json::Json;
+use rocket::serde::{Serialize, Deserialize};
+use rocket::fs::FileServer;
+
+// Temporary working structures
+#[derive(Deserialize)]
+struct InputCode
 {
-    // TODO: send back compiler file
-    "i work: ".to_owned() + &code
+    code: String,
+    lang: String,
+    options: String
+}
+
+#[derive(Serialize)]
+struct OutputCode
+{
+    stdin: String,
+    stdout: String,
+    result: Vec<u8>
+}
+
+#[post("/submit", format = "json", data = "<compilation_data>")]
+async fn post_submit(compilation_data: Json<InputCode>) 
+    -> Result<Json<OutputCode>, String> // TODO: Err should be some struct with detailed info
+{
+    // Input data validation
+    // TODO: validate input data: compilation options, language etc.
+
+    // Compilation goes here
+    let compilation_args = compilation_data.into_inner();
+    // TODO: call compiler here
+
+    // Handle compiler's result here
+    let some_result_path = "target/example/test.txt";
+    let compiled_data = std::fs::read(some_result_path);
+    
+    match compiled_data
+    {
+        Ok(bytes) => Ok(Json(OutputCode {
+            stdin: "some stdin".to_owned(),
+            stdout: "some stdout".to_owned(),
+            result: bytes
+        })),
+        Err(_) => Err("Error while reading compiler file.\
+            Compilation error or smthn, i dunno".to_owned())
+    }    
 }
 
 #[launch]
@@ -14,6 +54,6 @@ fn rocket() -> _
     // I'm not sure about FileServer, but it seems safe.
     // And official tutorials use it too
     rocket::build()
-        .mount("/", rocket::fs::FileServer::from("static/"))
+        .mount("/", FileServer::from("static/"))
         .mount("/", routes![post_submit])
 }
