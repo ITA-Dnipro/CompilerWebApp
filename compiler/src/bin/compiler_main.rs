b4c1e07767315c27714088e27955d0fc64d8e3ab
virtustron <virtustron@gmail.com>
2021-10-27 11:50:42 +0300

    Add handler::run_compilation usage to main fun

--- compiler/src/bin/compiler_main.rs
+++ compiler/src/bin/compiler_main.rs
@@ -1,14 +1,33 @@
 
 #![allow(unused)]
 
+extern crate compiler;
+
 use std::env;
 use std::process::Command;
 use std::io::{self, Write};
 
 
+use compiler::handler::run_compilation;
+use compiler::data::input_data::InputData;
+use compiler::data::input_data::compiler_type::CompilerType;
+
+
 fn main() -> std::io::Result<()> {
     println!("Hello from [compiler] crate!");
 
+    let input_data = InputData {
+        compiler_type: CompilerType::Cpp,
+        source_code: String::from("Here will be filepath to source code"),
+    };
+
+    let output_data = run_compilation(&input_data);
+
+    println!("Compiling status: {}", output_data.status_code.unwrap());
+    io::stdout().write_all(&output_data.stdout).unwrap();
+    io::stderr().write_all(&output_data.stderr).unwrap();
+    
+    
     // TODO remove compiled bin file from directory (call "rm")
     
     // TODO read about using env variables - in Ubuntu "export varName = varValue"
@@ -16,6 +35,7 @@ fn main() -> std::io::Result<()> {
 
     // TODO check g++ compiler installed
 
+    /*
     //
     let path = env::current_dir()?;
     
@@ -36,6 +56,8 @@ fn main() -> std::io::Result<()> {
     io::stderr().write_all(&output.stderr).unwrap();
 
     assert!(output.status.success()); 
-    
+    */
     Ok(())
+
+    
 }
