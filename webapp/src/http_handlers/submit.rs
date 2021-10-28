2c28f37b814b8b33bebf095e63416eb32365b33c
AndriiSerbeniuk <andriiserbeniuk@gmail.com>
2021-10-29 01:55:07 +0300

    Remade temp files handling

--- webapp/src/http_handlers/submit.rs
+++ webapp/src/http_handlers/submit.rs
@@ -1,32 +1,29 @@
 mod structs;
 
 use rocket::serde::json::Json;
+use std::fs::{File, remove_file};
 use std::io::{Write, Seek, SeekFrom};
 use structs::{InputCode, OutputCode};
+use std::env;
 
 #[post("/submit", format = "json", data = "<compilation_json>")]
 pub async fn post_submit(compilation_json: Json<InputCode>) 
     -> Result<Json<OutputCode>, String> // TODO: Err should be some struct with detailed info
 {
+    // TODO: respond with "internal server errors" explicitly?
     // Input data validation
     // TODO: validate input data: compilation options, language etc.
 
-
     // Compilation goes here
-    // If error ever happens on work with temp files it's not on the user, 
-    // so he should get "internal server error" here
-    // TODO: talk about how to better assign temp folder
-    // TODO: respond with "internal server error" explicitly?
-    let mut code_file = tempfile::tempfile().expect("Panic on temp file creation");  
-    code_file.write_all(compilation_json.code.as_bytes())
-        .expect("Panic on copying of source code into the temp file");
-    code_file.seek(SeekFrom::Start(0)).expect("Panic on temp file seek");
+    let source_file = write_source_to_file(&compilation_json.code,
+        &compilation_json.lang);
     // TODO: call compiler here
 
-    
-    drop(code_file);
+    remove_file(&source_file)
+        .expect("Panic on deletion of input source code file");
 
-    // Handle compiler's result here
+    // Handling of the result
+    // TODO: if result is Ok - write bin file into json, else respond with error messages
     let some_result_path = "target/example/test.txt";
     let compiled_data = std::fs::read(some_result_path)
         .expect("Panic on reading compiler output");
@@ -36,4 +33,28 @@ pub async fn post_submit(compilation_json: Json<InputCode>)
         stdout: "some stdout".to_owned(),
         result: compiled_data
     }))
+}
+
+fn write_source_to_file(source_code: &str, lang: &str) -> String
+{
+    // If error ever happens on work with temp files it's not on the user, 
+    // so he should get "internal server error" here
+    let mut input_file_name = env::var("COMPILATION_TEMP_DIR")
+        .expect("COMPILATION_TEMP_DIR doesn't exist");
+    input_file_name.push_str("/");
+    input_file_name.push_str(
+        &chrono::Utc::now()
+        .format("compilation_input-%Y-%m-%d-%H-%M-%S-%f").to_string()
+    );
+    input_file_name.push_str(".cpp");     // TODO: add lang specific extensions
+
+    let mut code_file = File::create(&input_file_name)
+        .expect("Panic on temp file creation");  
+    code_file.write_all(source_code.as_bytes())
+        .expect("Panic on copying of source code into the temp file");
+    code_file.seek(SeekFrom::Start(0)).expect("Panic on temp file seek");
+    
+    drop(code_file);
+
+    input_file_name
 }
--- webapp/src/main.rs
+++ webapp/src/main.rs
@@ -4,10 +4,16 @@ mod http_handlers;
 
 use http_handlers::submit;
 use rocket::fs::FileServer;
+use std::env::{set_var, current_dir};
 
 #[launch]
 fn rocket() -> _ 
 {
+    // Dir for compilation files
+    let mut temp_dir = current_dir().unwrap().to_str().unwrap().to_owned();
+    temp_dir.push_str("/tempdata");
+    set_var("COMPILATION_TEMP_DIR", temp_dir);
+
     rocket::build()
         .mount("/", FileServer::from("static/"))
         .mount("/", routes![submit::post_submit])
