da287c4437b6591b28e0e7bbb380e7fbbca57159
Oleksandr <n0naimatall@gmail.com>
2021-10-29 11:42:49 +0300

    Add file loading button

--- webapp/static/js/editor.js
+++ webapp/static/js/editor.js
@@ -1,3 +1,16 @@
 var editor = ace.edit("editor")
 editor.setTheme("ace/theme/chrome")
 editor.session.setMode("ace/mode/c_cpp")
+
+
+var reader = new FileReader()
+reader.onload = function(evt) {
+    editor.setValue(evt.target.result)
+}   
+function handleLoadedFile(value) {
+    var file = document.getElementById("file_input").files[0]
+    if (file) {
+        reader.readAsText(file, "UTF-8")
+    }
+}
+
