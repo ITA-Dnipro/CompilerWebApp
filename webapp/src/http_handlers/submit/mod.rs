291f2517cd2a0c9fe1bd01b1612b5fc957ab2c7e
AndriiSerbeniuk <andriiserbeniuk@gmail.com>
2021-11-04 16:40:51 +0200

    Worked on code review issues: - compilation handling is now done in a separate function; - improved error handling in the filework module, other minor improvements;

--- webapp/src/http_handlers/submit/mod.rs
+++ webapp/src/http_handlers/submit/mod.rs
@@ -10,15 +10,21 @@ use std::path::PathBuf;
 pub async fn post_submit(compilation_json: Json<structs::InputData>) 
     -> Json<structs::OutputData>
 {
+    Json(try_to_compile(compilation_json.into_inner()))
+}
+
+// TODO: change return type to Result when there'll be backend specific error cases
+fn try_to_compile(compiler_data: structs::InputData) -> structs::OutputData
+{
     // TODO: respond with "internal server errors" explicitly?
     // Input data validation
     // TODO: validate input data: compilation options, language etc.
 
     // Compilation goes here
-    let source_file = write_source_to_file(&compilation_json.code,
-        &compilation_json.lang);
+    let source_file = write_source_to_file(&compiler_data.code,
+        &compiler_data.lang).unwrap();
     let compiler_input = InputData::new(CompilerType::Cpp, PathBuf::from(&source_file),
-        PathBuf::from(source_file.parent().unwrap()), compilation_json.options.clone());
+        PathBuf::from(source_file.parent().unwrap()), compiler_data.options.clone());
     // Compiler call
     let compilation_result = run_compilation(&compiler_input);
 
@@ -29,6 +35,5 @@ pub async fn post_submit(compilation_json: Json<structs::InputData>)
         delete_file(&source_file.parent().unwrap().join(&compilation_result.compiled_file_name));
     }
 
-    // Handling of the result
-    Json(structs::OutputData::from_compiler_result(&compilation_result))
+    structs::OutputData::from_compiler_result(&compilation_result)
 }
