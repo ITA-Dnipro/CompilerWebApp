216ce09d883654b8d4755107b61e9de866490fe3
AndriiSerbeniuk <andriiserbeniuk@gmail.com>
2021-11-07 00:49:10 +0200

    Worked on code review issues: - files creation now can't panic in runtime; - post_submit() now responds with InternalServerError explicitly; - moved validator instantiation into try_to_compile().

--- webapp/src/http_handlers/submit/mod.rs
+++ webapp/src/http_handlers/submit/mod.rs
@@ -1,45 +1,60 @@
 mod structs;
 
-use std::path::PathBuf;
 use rocket::serde::json::Json;
-use super::super::filework::*;
+use rocket::response::status::Custom;
+use rocket::http::Status;
+use rocket::State;
+
 use compiler::data::input_data::{InputData, compiler_type::CompilerType};
 use compiler::handler::run_compilation;
-use super::super::languages::{lang_info::LangInfo, flags_validator::FlagsValidator};
 
-use rocket::State;
 use super::super::LangsInfo;
+use std::path::PathBuf;
+use super::super::filework::*;
+use super::super::languages::{lang_info::LangInfo, flags_validator::FlagsValidator};
 
 #[post("/submit", format = "json", data = "<compilation_json>")]
 pub async fn post_submit(compilation_json: Json<structs::InputData>,
-    langs_info: &State<LangsInfo>) -> Json<structs::OutputData>
+    langs_info: &State<LangsInfo>) 
+    -> Result<Json<structs::OutputData>, Custom<()>>
 {
     let compilation_data = compilation_json.into_inner();
     if langs_info.contains_key(&compilation_data.lang)
     {
         let lang_info = &langs_info[&compilation_data.lang];
-        let mut validator = FlagsValidator::new();
-        Json(try_to_compile(&compilation_data, &lang_info, &mut validator))
+        match try_to_compile(&compilation_data, &lang_info)
+        {
+            Ok(comp_result) => 
+                Ok(Json(comp_result)),
+            Err(_) => Err(Custom(Status::InternalServerError, ()))
+        }
     }
     else
     {
-        Json(structs::OutputData::new(-1, "", "Unknown language"))
+        Ok(Json(structs::OutputData::new(-1, "", "Unknown language")))
     }  
 }
 
-// TODO: change return type to Result when there'll be backend specific error cases
-fn try_to_compile(compilation_data: &structs::InputData, lang_info: &LangInfo, 
-    validator: &mut FlagsValidator) -> structs::OutputData
+fn try_to_compile(compilation_data: &structs::InputData, lang_info: &LangInfo)
+    -> Result<structs::OutputData, ()>
 {
+    let mut validator = FlagsValidator::new();
     // Input data validation
     if validator.validate(&compilation_data.options, &lang_info)
     {
-        // TODO: respond with "internal server errors" explicitly?
         // Compilation goes here
-        let source_file = write_source_to_file(&compilation_data.code,
-            &compilation_data.lang).unwrap();
-        let compiler_input = InputData::new(CompilerType::Cpp, PathBuf::from(&source_file),
-            PathBuf::from(source_file.parent().unwrap()), compilation_data.options.clone());
+        let source_file;
+        match write_source_to_file(&compilation_data.code,
+            &lang_info.lang_extension)
+        {
+            Some(path) => source_file = path,
+            None => return Err(())
+        }
+
+        let compiler_input = InputData::new(CompilerType::Cpp, 
+            PathBuf::from(&source_file),
+            PathBuf::from(source_file.parent().unwrap()), 
+            compilation_data.options.clone());
         // Compiler call
         let compilation_result = run_compilation(&compiler_input);
 
@@ -47,10 +62,11 @@ fn try_to_compile(compilation_data: &structs::InputData, lang_info: &LangInfo,
         if compilation_result.status_code.unwrap() == 0
         {
             // TODO: rework when compilation_result.compiled_file_name will contain full path
-            delete_file(&source_file.parent().unwrap().join(&compilation_result.compiled_file_name));
+            delete_file(&source_file.parent().unwrap()
+                .join(&compilation_result.compiled_file_name));
         }
 
-        structs::OutputData::from_compiler_result(&compilation_result)
+        Ok(structs::OutputData::from_compiler_result(&compilation_result))
     }
     else 
     {
@@ -60,7 +76,6 @@ fn try_to_compile(compilation_data: &structs::InputData, lang_info: &LangInfo,
         invalid_flags.pop();    // ' '
         invalid_flags.pop();    // ','
 
-        structs::OutputData::new(-1, "", 
-            &invalid_flags)
+        Ok(structs::OutputData::new(-1, "", &invalid_flags))
     }
 }
