56558fea135d6887746a8aceb8c878778690f861
Oleksandr <n0naimatall@gmail.com>
2021-11-07 19:31:24 +0200

    Add rust as second option. Add rust syntax highlight.

--- webapp/static/js/editor.js
+++ webapp/static/js/editor.js
@@ -15,3 +15,19 @@ function handleLoadedFile(value) {
     }
 }
 
+function setEditorMode(event) {
+    console.log(event)
+    switch (event.target.value) {
+        case 'c++':
+            editor.session.setMode('ace/mode/c_cpp')
+            break
+
+        case 'rust':
+            editor.session.setMode('ace/mode/rust')
+            break
+    }
+}
+
+document
+    .getElementById('lang-input')
+    .addEventListener('change', setEditorMode)
