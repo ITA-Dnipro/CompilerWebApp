4d60a6acf526ccad4fadf8ed7ca8a8d798d81ecc
Oleksandr <n0naimatall@gmail.com>
2021-11-24 17:13:36 +0200

    Filter mod: Do some refactorings. Add some tests.
    Change filter config file name from FILTERS_CONFIG_PATH to CWA_FILTERS_CONFIG_PATH.
    Get rid of unwrap statements, return Err.
    Add following tests:
    1) no_such_preset - test if build_filter returns Err on brocken preset.
    2) broken_json - test if build_filter returns Err on brocken json config.
    3) error_message - test if build_filter return Err on on config file path.

--- runner/lib/filter/mod.rs
+++ runner/lib/filter/mod.rs
@@ -1,13 +1,15 @@
 use std::fs::{File};
 use std::path::Path;
 use std::env;
+use std::string::String;
 use seccompiler::{compile_from_json, BpfProgram, TargetArch};
 
 const FILTERS_CONFIG_PATH: &str = "config/filters.json";
 
-pub fn build_filter() -> Result<BpfProgram, &'static str> {
-    // TODO: read json path from env variable, change default one
-    let config_path = match env::var("FILTERS_CONFIG_PATH") {
+pub fn build_filter() -> Result<BpfProgram, String> {
+    // TODO: change default json_path
+    let preset_name= String::from("default");
+    let config_path = match env::var("CWA_FILTERS_CONFIG_PATH") {
         Ok(env_path) => { 
             env_path 
         },
@@ -15,13 +17,63 @@ pub fn build_filter() -> Result<BpfProgram, &'static str> {
             String::from(FILTERS_CONFIG_PATH)
         }
     };
-    assert!(Path::new(config_path.as_str()).exists());
-    let mut filters = compile_from_json(
+    if !Path::new(config_path.as_str()).exists() {
+        return Err(format!("{}: no such file.", config_path))
+    }
+    let mut _filters = compile_from_json(
         File::open(config_path.as_str()).unwrap(), 
         TargetArch::x86_64
-    ).expect("Cannot compile filters from json");
-    // TODO: use preset string as func param
-    let bpf_prg: BpfProgram = filters.remove("main_thread").unwrap();
+    );
+    let mut filters = match _filters {
+        Ok(_filt) => _filt,
+        Err(error) => {
+            return Err(error.to_string())
+        }
+    };
     
-    return Ok(bpf_prg);
+    // TODO: use preset string as func param
+    if let Some(bpf_prg) = filters.remove(preset_name.as_str()) {
+        Ok(bpf_prg)
+    } else {
+        Err(
+            format!(
+                "{}: no such preset in {}", 
+                preset_name, 
+                config_path
+            )
+        )
+    }
+}
+
+
+#[test]
+fn error_message() {
+    let expected_path = "some non-existing file";
+    env::set_var("CWA_FILTERS_CONFIG_PATH", expected_path);
+    if let Err(error) =  build_filter() {
+        assert_eq!(format!("{}: no such file.", expected_path), error);
+    };
+}
+
+#[test]
+fn broken_json() {
+    let expected_path = "test/data/test.json";
+    env::set_var("CWA_FILTERS_CONFIG_PATH", expected_path);
+    if let Err(error) = build_filter() {
+        assert!(
+            error.contains("Json Frontend error:")
+        );       
+    };
+}
+
+#[test]
+fn no_such_preset() {
+    let expected_path = "test/data/no_such_preset.json";
+    env::set_var("CWA_FILTERS_CONFIG_PATH", expected_path);
+    if let Err(error) = build_filter() {
+        assert_eq!(
+            format!("{}: no such preset in {}", "default", expected_path),
+            error
+        );       
+    };
 }
