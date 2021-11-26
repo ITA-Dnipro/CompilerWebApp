29ace4d057bf46dbf9f5b18b8574e5600b919011
AndriiSerbeniuk <andriiserbeniuk@gmail.com>
2021-11-26 10:38:11 +0200

    impl submit headers reading #48

--- webapp/src/http_handlers/submit/structs.rs
+++ webapp/src/http_handlers/submit/structs.rs
@@ -4,6 +4,8 @@ use compiler::data::output_data;
 
 pub mod submit_input;
 pub mod submit_output;
+pub mod submit_headers;
 
 pub use submit_input::SubmitInput;
 pub use submit_output::SubmitOutput;
+pub use submit_headers::SubmitHeaders;
